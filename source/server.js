01def43ab7bc9dc1cb4b705955bf400b57e15b7f
Jose Avilez <jose.avilez@partnerhero.com>
2019-09-28 15:47:50 -0600

    Update server.js
    Updated the console loggin when the server starts.

--- source/server.js
+++ source/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-db.on('error', error => console.log('Error ocurred: ', error));
+db.on('error', error => console.log('Error ocurred while connecting to DB: ', error));
 db.once('open', () => console.log('Connected to the DB.'));
 
 app.use(express.json());
@@ -15,4 +15,4 @@ app.use(express.json());
 const subscribersRouter = require('./routes/subscribers');
 app.use('/subscribers', subscribersRouter);
 
-app.listen(process.env.PORT, () => console.log('server started'));
+app.listen(process.env.PORT, () => console.log('Server Started at', process.env.PORT));
